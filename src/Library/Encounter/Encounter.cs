57a9243a4fb9632c01d37fa447600934b14278b8
Franloc <francesco.locatelli@correo.ucu.edu.uy>
2026-05-11 20:48:49 -0300

    inicio de encounter

--- src/Library/Encounter/Encounter.cs
+++ src/Library/Encounter/Encounter.cs
@@ -4,10 +4,36 @@ namespace Ucu.Poo.RolePlayGame
 {
     public class Encounter
     {
-        private List <Character> characters = new List<Character>();
-        public List <Character> Characters
+        private List <Hero> heroes = new List<Hero>();
+        public List <Hero> Heroes
         {
-            get {return this.characters;} set {this.characters = value;}
+            get {return this.heroes;} set {this.heroes = value;}
+        }
+        private List <Enemy> enemies = new List<Enemy>();
+        public List <Enemy> Enemies
+        {
+            get {return this.enemies;} set {this.enemies = value;}
+        }
+        public void DoEncounter()
+        {
+            if (this.enemies.Count>0 && this.heroes.Count>0)
+            {
+                if (this.heroes.Count==1)
+                {
+                    foreach (Enemy enemy in enemies)
+                    {
+                        heroes[0].ReceiveAttack(enemy);
+                    }
+                }
+                else
+                {
+                    for (int i=0;i<=enemies.Count;i++)
+                    {
+                        heroes[i%heroes.Count].ReceiveAttack(enemies[i]);
+                    }
+                }
+                
+            }
         }
     }
 }

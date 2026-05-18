ea5058698e4eab3ca63a8b4b60ffd2996f0dde8d
UCUBascou <nicolas.bascou@correo.ucu.edu.uy>
2026-05-18 00:44:23 -0300

    Update Encounter.cs

--- src/Library/Encounter/Encounter.cs
+++ src/Library/Encounter/Encounter.cs
@@ -40,6 +40,8 @@ namespace Ucu.Poo.RolePlayGame
                     for (int i=0;i<enemies.Count;i++)
                     {
                         heroes[i%heroes.Count].ReceiveAttack(enemies[i]);
+
+                        //Comprobar si murio, en tal caso eliminar al heroe de la batalla.
                         if (heroes[i%heroes.Count].Health<=0)
                             {
                                 heroes[i%heroes.Count].Remove(hero);
@@ -49,15 +51,15 @@ namespace Ucu.Poo.RolePlayGame
 
 
 
-                
+                //Recorre cada heroe, y cada uno ataca a cada enemigo
                 foreach (Hero hero in heroes)
                 {
                     if (hero.Health > 0)
                     {
                         foreach (Enemy enemy in enemies)
                         {
-
                             enemy.ReceiveAttack(hero);
+                            
                             //Comprobar si murio, en tal caso darle los puntos de victoria al heroe y eliminar el enemigo de la batalla.
                             if (enemy.Health<=0)
                             {
@@ -65,20 +67,21 @@ namespace Ucu.Poo.RolePlayGame
                                 enemies.Remove(enemy);
                             }         
                         }
-                    }
-
-
-                    
+                    } 
                 }
             }
+
+            //Cada heroe que haya ganado mas de 5 puntos de victoria al terminar el encuentro se cura.
             foreach (Hero hero in heroes)
             {
-                if (hero.AccumulatedVictoryPoints>=5)
+                if (hero.AccumulatedVictoryPoints - hero.BaseVictoryPoints >=5)
                 {
                     hero.Cure();
                 }
             }
         }
+
+        //Constructor
         public Encounter(List<Hero> heroes, List<Enemy> enemies)
         {
             this.heroes = heroes;
